c00106a7b520522d9c2b421ea5e95db2f8a23400
dibrinsofor <dibrinsofor@gmail.com>
2023-08-02 15:17:01 +0100

    linked build specs to parser
    Signed-off-by: dibrinsofor <dibrinsofor@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/dibrinsofor/chase
 
 go 1.18
+
+require (
+	github.com/alecthomas/participle v0.7.1
+	github.com/alecthomas/participle/v2 v2.0.0 // indirect
+	github.com/alecthomas/repr v0.2.0
+)
--- main.go
+++ main.go
@@ -1,16 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"runtime"
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-    }
-}
+	"github.com/alecthomas/repr"
 
-type T struct {
+	"github.com/dibrinsofor/chase/src"
+)
 
+// todo: add file watcher to detect chasefile in root directory and generate dependency graph
+
+var filename = "Chasefile"
+
+type env struct {
+	shell []string
+	vars  map[string]string
 }
 
 func main() {
+	// e := env{}
 
+	// check if chasefile exists
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) || info.IsDir() || err != nil {
+		panic(fmt.Errorf("chase: error opening chasefile: %w", err))
+	}
+
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		panic(fmt.Errorf("chase: error opening chasefile: %w", err))
+	}
+
+	// do we want to do any linting or add. checks before attempting to parse?
+	ast, err := src.ChasefileParser.ParseBytes(filename, b)
+	if err != nil {
+		panic(fmt.Errorf("chase: error parsing chasefile: %w", err))
+	}
+
+	repr.Println(ast)
+
+	// if no "set shell" then try sh
+	if runtime.GOOS == "windows" {
+		// redirect to path for windows
+	}
 }
